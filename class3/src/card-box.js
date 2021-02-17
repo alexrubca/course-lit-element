0bdeb178cec869edbe06c68e78d6f7cbc0fe8ea5
Alejandro Rubio Calvente <alejandro.rubio.calvente.contractor@bbva.com>
2021-02-17 14:40:52 +0100

    feat(*): add files of class 3

---
+++ class3/src/card-box.js
@@ -0,0 +1,41 @@
+import { LitElement, html, css } from 'lit-element';
+import { sharedStyles } from './shared-styles';
+
+export class CardBox extends LitElement {
+
+  static get styles() {
+    return [sharedStyles, css`
+      :host {
+        display: block;
+        border: 1px solid #ddd;
+      }
+      :host([hidden]) {
+        display: none;
+      }
+      :host(.red) {
+        border-color: red;
+      }
+      :host(.red) span {
+        color: red;
+      }
+      ::slotted(a) {
+        background-color: red;
+      }
+      h1 {
+        font-size: 1.2;
+        font-weight: 300;
+        margin-top: 0;
+        color: var(--card-box-title-color, #888);
+        background-color: var(--card-box-title-background-color, transparent)
+      }
+    `];
+  }
+
+  render() {
+    return html`
+      ${this.title ? html`<h1>${this.title}</h1>` : '' }
+      <slot></slot>
+    `;
+  }
+}
+customElements.define('card-box', CardBox);
---
+++ class3/src/icons.js
@@ -0,0 +1,7 @@
+import { html } from 'lit-element';
+
+export const icons = {
+  done: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg>',
+  bookmark: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z"/></svg>',
+  user: html`<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>`,
+};
